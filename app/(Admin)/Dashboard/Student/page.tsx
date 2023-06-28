fc0bcffe788eb7f4cf4ff8a753f7f0ce0a799d8d
Swapnil1530 <patelswapnil1004@gmail.com>
2023-06-28 21:22:19 +0530

    feat/Admin panel

---
+++ app/(Admin)/Dashboard/Student/page.tsx
@@ -0,0 +1,26 @@
+import StudentTable from "../../../../components/table";
+import Form from "components/form";
+
+const datas = async () => {
+    const res = await fetch("http://localhost:3000/api/Admin", {
+        cache: 'no-store',
+    });
+    return res.json();
+};
+
+const Student = async () => {
+    const docs = await datas();
+
+    return (
+        <>
+
+            <Form type="register"/>
+
+            <div className="flex flex-col items-center justify-center">
+                <StudentTable studentData={docs}/>
+            </div>
+        </>
+    );
+};
+
+export default Student;
